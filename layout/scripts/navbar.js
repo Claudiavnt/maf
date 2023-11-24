60ef1f23f86178d85853fb42d7785e06398b1524
Claudia Ventura <c.ventura@quidinfo.it>
2023-11-24 10:25:00 +0100

    menu responsive

---
+++ layout/scripts/navbar.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var navbarContainer = document.getElementById('mainav');
+
+  var currentPage = window.location.pathname.split('/').pop(); // Ottieni il nome della pagina corrente
+    var listItem = document.createElement('ul');
+    listItem.className = 'clear mainav-inline';
+    listItem.innerHTML = `
+      <li class="${currentPage === 'index.html' ? 'active' : ''}"><a href="index.html">Home</a></li>
+      <li class="${currentPage === 'visita.html' ? 'active' : ''}"><a href="visita.html">Come Visitare</a></li>
+      <li class="${currentPage === 'eventi.html' ? 'active' : ''}"><a href="#">Eventi</a></li>
+      <li class="${currentPage === 'educazione.html' ? 'active' : ''}"><a href="#">Educazione</a></li>
+      <li class="${currentPage === 'child.html' ? 'active' : ''}"><a href="#">MAF CHILD</a></li>
+      <li class="${currentPage === 'podcast.html' ? 'active' : ''}"><a href="#">Podcast</a></li>
+    `;
+    navbarContainer.appendChild(listItem);
+});
